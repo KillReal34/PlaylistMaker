459b07a99c2122f854a4fd91c8189380df6fb0d3
KillReal34 <Kimate2499@yandex.ru>
2023-11-19 22:00:33 +0300

    Added dark theme for all activities Added functionality for activities settings Added functionality for search activity

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,67 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputSearchText = findViewById<EditText>(R.id.inputSearchText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val backButton = findViewById<Button>(R.id.button_back_activitySearch)
+
+        backButton.setOnClickListener{
+            onBackPressed()
+        }
+
+        clearButton.setOnClickListener {
+            inputSearchText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                SEARCH_TEXT = s.toString()
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //empty
+            }
+        }
+        inputSearchText.setText(SEARCH_TEXT)
+        inputSearchText.addTextChangedListener(simpleTextWatcher)
+
+    }
+    companion object {
+        var SEARCH_TEXT = ""
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("text", SEARCH_TEXT)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.getString(SEARCH_TEXT)
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,14 @@
 package com.example.playlistmaker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +18,43 @@ class SettingsActivity : AppCompatActivity() {
         val buttonBackMain = findViewById<Button>(R.id.button_back)
         buttonBackMain.setOnClickListener{
             onBackPressed()
+        }
+
+        val switchDarkTheme = findViewById<SwitchCompat>(R.id.switch_dark_theme)
+        switchDarkTheme.setOnCheckedChangeListener{buttonView, isChecked ->
+            if (isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        val shareApp = findViewById<TextView>(R.id.text_share_app)
+        shareApp.setOnClickListener{
+            val sendShareAppIntent = Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_headerText))
+                type = "text/plain"
+            }
+            startActivity(Intent.createChooser(sendShareAppIntent, getString(R.string.share_app_message)))
+        }
+
+        val supportText = findViewById<TextView>(R.id.text_support)
+        supportText.setOnClickListener{
+            val sendSupportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_address)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.support_message))
+            }
+            startActivity(sendSupportIntent)
+        }
+
+        val userAgreementText = findViewById<TextView>(R.id.text_user_agreement)
+        userAgreementText.setOnClickListener{
+            val sendUserAgreementIntent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(getString(R.string.user_agreement_address))
+            }
+            startActivity(sendUserAgreementIntent)
         }
     }
 }
