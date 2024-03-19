27375db8c4e790c501ce73952a00d47cfe02005b
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2024-03-19 23:12:53 +0300

    Add Interactors for Search Screen

---
+++ app/src/main/java/com/example/playlistmaker/search/domain/interactor/SearchTracksByNameInteractor.kt
@@ -0,0 +1,7 @@
+package com.example.playlistmaker.search.domain.interactor
+
+import com.example.playlistmaker.domain.entities.Track
+
+interface SearchTracksByNameInteractor {
+    operator fun invoke(namePattern: String): List<Track>
+}
