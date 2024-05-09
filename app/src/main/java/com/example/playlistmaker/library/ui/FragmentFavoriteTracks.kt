1def4b7c5cfea3e8c1291430db8db4580b0b841c
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2024-05-09 16:01:05 +0300

    Description of fragments Layout fragments and activity_library

--- app/src/main/java/com/example/playlistmaker/library/ui/FragmentFavoriteTracks.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/FragmentFavoriteTracks.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentFavoriteTracksBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FragmentFavoriteTracks : Fragment() {
 
@@ -13,19 +17,36 @@ class FragmentFavoriteTracks : Fragment() {
         private const val TRACKS = "track_list"
 
         fun newInstance(tracks: String) = FragmentFavoriteTracks().apply {
-            arguments = Bundle().apply {
-                putString(TRACKS, tracks)
-            }
+            arguments = bundleOf(TRACKS to tracks)
         }
     }
 
-    private lateinit var binding: FragmentFavoriteTracksBinding
+    private val fragmentFavoriteTracksViewModel: FragmentFavoriteTracksViewModel by viewModel()
+
+    private val binding by lazy(mode = LazyThreadSafetyMode.NONE) {
+        FragmentFavoriteTracksBinding.inflate(layoutInflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentFavoriteTracksBinding.inflate(inflater, container, false)
-        return binding.root
+    ): View = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        withBinding {
+            with(ivErrorImage) {
+                val imageResId =
+                    if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                        R.drawable.not_found_track_image_night_mode
+                    } else {
+                        R.drawable.not_found_track_image_light_mode
+                    }
+                setImageResource(imageResId)
+            }
+        }
     }
+
+    private inline fun <R> withBinding(action: FragmentFavoriteTracksBinding.() -> R) =
+        binding.action()
 }
--- app/src/main/java/com/example/playlistmaker/library/ui/LibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/LibraryActivity.kt
@@ -1,12 +1,42 @@
 package com.example.playlistmaker.library.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.playlistmaker.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.playlistmaker.databinding.ActivityLibraryBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LibraryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLibraryBinding
+
+    private lateinit var tabMediator: TabLayoutMediator
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_library)
+        binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val tracks = "tracks"
+        val playlists = "playlists"
+
+        binding.buttonBack.setOnClickListener{
+            onBackPressed()
+        }
+
+        binding.viewPager.adapter =
+            LibraryViewPagerAdapter(supportFragmentManager, lifecycle, tracks, playlists)
+
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) {tab, position ->
+            when(position) {
+                0 -> tab.text = "Избранные треки"
+                1 -> tab.text = "Плейлисты"
+            }
+        }
+        tabMediator.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
     }
 }
