53f886fa022d1c5f929287e3478a90c2eef9f3c1
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2024-12-02 22:35:59 +0300

    Add new dependency in DI

--- app/src/main/java/com/example/playlistmaker/di/DataModule.kt
+++ app/src/main/java/com/example/playlistmaker/di/DataModule.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker.di
 
 import androidx.room.Room
+import com.example.playlistmaker.creationPlaylistWindow.data.converters.PlaylistDatabaseConverter
 import com.example.playlistmaker.library.data.db.AppDatabase
 import com.example.playlistmaker.player.data.AndroidMediaPlayer
 import com.example.playlistmaker.player.domain.entities.SimplePlayer
+import com.google.gson.Gson
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
@@ -11,8 +13,13 @@ import org.koin.dsl.module
 val dataModule = module {
     factoryOf<SimplePlayer>(::AndroidMediaPlayer)
 
-    single{
-        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db").build()
+    factory{ PlaylistDatabaseConverter(get()) }
+
+    factory{Gson()}
+
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db").fallbackToDestructiveMigration()
+            .build()
     }
 
     includes(persistenceModule, networkModule)
