dfd881736def50d24fa7b3c03182c0f087aeed27
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2024-12-24 13:27:29 +0300

    Add new methods in PlaylistRepository.kt from SelectedPlaylistInteractor

--- app/src/main/java/com/example/playlistmaker/library/domain/repository/PlaylistRepository.kt
+++ app/src/main/java/com/example/playlistmaker/library/domain/repository/PlaylistRepository.kt
@@ -1,9 +1,18 @@
 package com.example.playlistmaker.library.domain.repository
 
 import com.example.playlistmaker.creationPlaylistWindow.domain.model.Playlist
+import com.example.playlistmaker.domain.entities.Track
 import kotlinx.coroutines.flow.Flow
 
 
 interface PlaylistRepository {
     suspend fun getPlaylist(): Flow<List<Playlist>>
+
+    fun getPlaylistById(playlistId: Long): Flow<Playlist>
+
+    fun getTrackFormPlaylist(playlistIdList: List<Int>): Flow<List<Track>>
+
+    fun deleteTrackById(trackId: Int)
+
+    fun deletePlalistById(playlistId: Long)
 }
