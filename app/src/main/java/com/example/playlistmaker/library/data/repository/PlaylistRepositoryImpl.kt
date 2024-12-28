dbe526693dab85590e4aa542aaede34b6892d0e1
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2024-12-28 16:04:56 +0300

    Add override methods from PlaylistRepositoryImpl.kt

--- app/src/main/java/com/example/playlistmaker/library/data/repository/PlaylistRepositoryImpl.kt
+++ app/src/main/java/com/example/playlistmaker/library/data/repository/PlaylistRepositoryImpl.kt
@@ -2,9 +2,11 @@ package com.example.playlistmaker.library.data.repository
 
 import com.example.playlistmaker.creationPlaylistWindow.data.converters.PlaylistDatabaseConverter
 import com.example.playlistmaker.creationPlaylistWindow.domain.model.Playlist
+import com.example.playlistmaker.domain.entities.Track
 import com.example.playlistmaker.library.data.db.AppDatabase
 import com.example.playlistmaker.library.domain.repository.PlaylistRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 
 class PlaylistsRepositoryImpl(
@@ -14,5 +16,22 @@ class PlaylistsRepositoryImpl(
         return appDatabase.playlistDao().getPlaylists().map { list ->
             list.map { convertor.convertorPlaylistEntityFromPlaylist(it) }
         }
+    }
+
+    override fun getPlaylistById(playlistId: Long): Flow<Playlist> = flow{
+        val playlist = appDatabase.playlistDao().getPlaylistById(playlistId)
+        emit(convertor.convertorPlaylistEntityFromPlaylist(playlist))
+    }
+
+    override fun getTrackFormPlaylist(playlistIdList: List<Int>): Flow<List<Track>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteTrackById(trackId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deletePlaylistById(playlistId: Long) {
+        TODO("Not yet implemented")
     }
 }
