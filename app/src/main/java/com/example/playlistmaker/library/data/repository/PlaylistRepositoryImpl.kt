1b645ad3aaf04afffccae92af6524bcc96cb445d
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2024-12-29 23:02:51 +0300

    Implementation method getTrackFromPlaylist Add method trackFilter

--- app/src/main/java/com/example/playlistmaker/library/data/repository/PlaylistRepositoryImpl.kt
+++ app/src/main/java/com/example/playlistmaker/library/data/repository/PlaylistRepositoryImpl.kt
@@ -5,26 +5,31 @@ import com.example.playlistmaker.creationPlaylistWindow.domain.model.Playlist
 import com.example.playlistmaker.domain.entities.Track
 import com.example.playlistmaker.library.data.db.AppDatabase
 import com.example.playlistmaker.library.domain.repository.PlaylistRepository
+import com.example.playlistmaker.player.data.TrackPlaylistEntity
+import com.example.playlistmaker.player.data.converters.TrackPlaylistConverter
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 
 class PlaylistsRepositoryImpl(
     private val appDatabase: AppDatabase,
-    private val convertor: PlaylistDatabaseConverter): PlaylistRepository {
+    private val convertor: PlaylistDatabaseConverter,
+    private val trackPlaylistConverter: TrackPlaylistConverter): PlaylistRepository {
     override suspend fun getPlaylist(): Flow<List<Playlist>> {
         return appDatabase.playlistDao().getPlaylists().map { list ->
             list.map { convertor.convertorPlaylistEntityFromPlaylist(it) }
         }
     }
 
-    override fun getPlaylistById(playlistId: Long): Flow<Playlist> = flow{
+    override fun getPlaylistById(playlistId: Long): Flow<Playlist> = flow {
         val playlist = appDatabase.playlistDao().getPlaylistById(playlistId)
         emit(convertor.convertorPlaylistEntityFromPlaylist(playlist))
     }
 
-    override fun getTrackFormPlaylist(playlistIdList: List<Int>): Flow<List<Track>> {
-        TODO("Not yet implemented")
+    override fun getTrackFromPlaylist(playlistIdList: List<Int>): Flow<List<Track>> {
+        return appDatabase.playlistDao().getAllTracksFromPlaylist().map { listEntity ->
+            trackFilter(listEntity, playlistIdList)
+        }
     }
 
     override fun deleteTrackById(trackId: Int) {
@@ -33,5 +38,16 @@ class PlaylistsRepositoryImpl(
 
     override fun deletePlaylistById(playlistId: Long) {
         TODO("Not yet implemented")
+    }
+
+    private fun trackFilter(
+        playlistEntity: List<TrackPlaylistEntity>,
+        playlistIdList: List<Int>
+    ): List<Track> {
+        val result = mutableListOf<Track>()
+        for (track: TrackPlaylistEntity in playlistEntity)
+            if (playlistIdList.any { it == track.trackId.toInt() })
+            result.add(trackPlaylistConverter.converterTrackPlaylistEntityFromTrack(track))
+        return result
     }
 }
