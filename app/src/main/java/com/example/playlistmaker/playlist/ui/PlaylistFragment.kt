257e184c80653e0275271da10ea127a24bd7bf55
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2025-01-03 18:22:40 +0300

    Add call viewModel Add method createArgs

--- app/src/main/java/com/example/playlistmaker/playlist/ui/PlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/playlist/ui/PlaylistFragment.kt
@@ -1,4 +1,18 @@
 package com.example.playlistmaker.playlist.ui
 
-class PlaylistFragment {
+import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class PlaylistFragment: Fragment() {
+
+    companion object{
+        private const val ARGS_PLAYLIST_ID = "playlist_id"
+        fun createArgs(playlistId: Long): Bundle =
+            bundleOf(ARGS_PLAYLIST_ID to playlistId)
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
+    }
+
+    private val viewModel: PlaylistViewModel by viewModel()
 }
