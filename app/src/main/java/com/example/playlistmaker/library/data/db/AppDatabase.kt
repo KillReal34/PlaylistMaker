84d36e5c1780802568b38bcf7167235d14613a2a
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2025-01-06 02:26:40 +0300

    Update version database

--- app/src/main/java/com/example/playlistmaker/library/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/playlistmaker/library/data/db/AppDatabase.kt
@@ -7,7 +7,7 @@ import com.example.playlistmaker.creationPlaylistWindow.data.db.PlaylistEntity
 import com.example.playlistmaker.player.data.TrackPlaylistEntity
 import com.example.playlistmaker.player.data.dao.TrackPlaylistDao
 
-@Database(version = 3, entities = [TrackEntity::class, PlaylistEntity::class, TrackPlaylistEntity::class])
+@Database(version = 5, entities = [TrackEntity::class, PlaylistEntity::class, TrackPlaylistEntity::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun trackDao(): TrackDao
 
