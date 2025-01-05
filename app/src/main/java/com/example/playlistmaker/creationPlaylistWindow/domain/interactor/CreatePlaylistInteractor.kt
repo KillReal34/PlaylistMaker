c9b35f2cf753f3b4689c3ab9385ee666c5026c18
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2025-01-06 02:24:16 +0300

    Add method addTrackInPlaylist

--- app/src/main/java/com/example/playlistmaker/creationPlaylistWindow/domain/interactor/CreatePlaylistInteractor.kt
+++ app/src/main/java/com/example/playlistmaker/creationPlaylistWindow/domain/interactor/CreatePlaylistInteractor.kt
@@ -1,10 +1,13 @@
 package com.example.playlistmaker.creationPlaylistWindow.domain.interactor
 
 import com.example.playlistmaker.creationPlaylistWindow.domain.model.Playlist
+import com.example.playlistmaker.domain.entities.Track
 import kotlinx.coroutines.flow.Flow
 
 interface CreatePlaylistInteractor {
     fun addPlaylist(playlist: Playlist): Flow<Long>
+
+    fun addTrackInPlaylist(track: Track)
 
     fun updatePlaylist(playlist: Playlist): Flow<Int>
 
