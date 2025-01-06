704952a7d8f5b1aaffc345fe2c97e04a97d359d3
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2025-01-06 03:11:14 +0300

    Rename method Take open method checkPlaylist()

--- app/src/main/java/com/example/playlistmaker/creationPlaylistWindow/ui/CreationPlaylistViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/creationPlaylistWindow/ui/CreationPlaylistViewModel.kt
@@ -9,11 +9,11 @@ import com.example.playlistmaker.creationPlaylistWindow.domain.model.Playlist
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class CreationPlaylistViewModel(
+open class CreationPlaylistViewModel(
     private val interactor: CreatePlaylistInteractor
 ) : ViewModel() {
 
-    var CreateNewPlaylistFragment = false
+    var createNewPlaylistFragment = false
     var playlistUri = ""
     var playlistDescription = ""
     var playlistName = ""
@@ -46,11 +46,11 @@ class CreationPlaylistViewModel(
                 }
             }
         }
-        CreateNewPlaylistFragment = false
+        createNewPlaylistFragment = false
     }
 
-    fun checkPlaylist() {
-        CreateNewPlaylistFragment =
+    open fun checkPlaylist() {
+        createNewPlaylistFragment =
             playlistName.isNotEmpty() || playlistDescription.isNotEmpty() || playlistUri.isNotEmpty()
     }
 
